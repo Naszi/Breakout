1f0ff3e45f5c5a29d3cb6692be99ab768e43d00f
Naszi <naszi75@gmail.com>
2016-02-04 15:41:50 +0100

    Version 6

--- Breakout/src/hu/uni/miskolc/naszi/Breakout.java
+++ Breakout/src/hu/uni/miskolc/naszi/Breakout.java
@@ -1,10 +1,32 @@
 package hu.uni.miskolc.naszi;
 
-public class Breakout {
+import java.awt.EventQueue;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import javax.swing.JFrame;
 
+public class Breakout extends JFrame {
+	public Breakout() {
+		initUI();
 	}
 
+	private void initUI() {
+		add(new Board());
+		setTitle("Breakout");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(Commons.WIDTH, Commons.HEIGHT);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				Breakout game = new Breakout();
+				game.setVisible(true);
+			}
+		});
+	}
 }
